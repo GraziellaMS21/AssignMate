954a912e624baa5f653d1c13466d52b70ab41b6a
swaghero1 <irisjuhraaa@gmail.com>
2025-11-22 17:28:36 +0800

    create auth and mainpage with sqlite db

--- app/src/main/java/com/example/assignmate/MainActivity.kt
+++ app/src/main/java/com/example/assignmate/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.assignmate
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.assignmate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // You can now access your views via the binding object, for example:
+        // binding.welcomeMessage.text = "Welcome, user!"
     }
 }
